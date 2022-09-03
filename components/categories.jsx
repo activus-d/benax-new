8cc2519b5a43dfd21fbdb84f2a6492b51cb14de3
D.M. Oladele <damilolamohammed2@hotmail.com>
2022-09-03 21:35:33 +0100

    categories font weight increased

--- components/categories.jsx
+++ components/categories.jsx
@@ -3,7 +3,7 @@ import {BsArrowRight} from 'react-icons/bs'
 
 export default function Categories() {
     return (
-        <section className='pt-24 px-5 md:px-0 md:pl-14 md:mr-14 basis-1/2'>
+        <section className='pt-24 px-5 md:px-0 md:pl-14 md:mr-14 basis-1/2 font-bold'>
             <h2 className='text-3xl mb-5 text-deepBlue'>CATEGORIES</h2>
             <ul>
                 <li className='flex items-center justify-between h-14 border-t border-t-lightGrey hover:text-lightGrey cursor-pointer'>
