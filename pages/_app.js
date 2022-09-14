c2a40378e9658b2b09f1d7a97ee796143016fa78
D.M. Oladele <damilolamohammed2@hotmail.com>
2022-09-14 20:32:08 +0100

    cart trial2

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
+import {
+   useQuery,
+   useMutation,
+   useQueryClient,
+   QueryClient,
+   QueryClientProvider,
+ } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import '../styles/globals.css'
 import { GlobalProvider } from '../components/globalContext'
 import Layout from '../components/layout'
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GlobalProvider>
+    <QueryClientProvider client={queryClient}>
+      <GlobalProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+      </GlobalProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -5,50 +5,59 @@ import { useGlobalContext } from "../components/globalContext";
 const Cart = ({cloths}) => {
     const {cartItems} = useGlobalContext()
     const [items, setItems] = useState([])
-    console.log(cartItems)
-    console.log(cloths)
-    // const fetchData = async (id, category) => {
-    //     const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/${category}s/${id}?populate=*`)
-    //     const data = await response.json()
-    //     setItems((prevVal) => {
-    //         return [...prevVal, data]
+    const collection = []
+    // console.log(cartItems)
+    // console.log(cloths)
+    // const fetchData = () => {
+    //     cartItems.forEach((item) => {
+    //         const {id, category} = item
+    //         fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/${category}s/${id}?populate=*`)
+    //             .then(res => res.json())
+    //             .then(data => collection.push(data))
+    //         // setItems([...items, data])
+    //         console.log(collection)
     //     })
     // }
+    
+
+    const fetchData = async () => {
+        const response =  await Promise.all(cartItems.map(item => {
+            const {id, category} = item
+            fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/${category}s/${id}?populate=*`)
+                .then(res => res.json())
+                .then(data => data)
+        }))
+         
+            
+            
+        // setItems([...items, data])
+        console.log(response)
+    }
 
     useEffect(() => {
-        cartItems.forEach(item => {
-            const {id, category} = item
-            // fetchData(id, category)
-            fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/${category}s/${id}?populate=*`)
-                .then(response => response.json())
-                .then(data => {
-                    setItems((prevVal) => {
-                        return [...prevVal, data]
-                    })
-                })
-        });
-        console.log(items)
+        fetchData()
+        
+        // console.log(items)
     }, [])
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         console.log(items)
+    //     }, 2000)
+    //     return () => {
+    //         clearInterval(interval)
+    //     }
+    // })
     
     
     return (
         <section>
-            {items.map(item => {
+            {/* {items.map(item => {
                 return <div>{item.data.attributes.product_name}</div>
-            })}
+            })} */}
         </section>
     )
 }
-
-// export async function getStaticProps() {
-//     const clothsResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/cloths/1/?populate=*`);
-//     // console.log(process.env.NEXT_PUBLIC_STRAPI_URL)
-//     return {
-//         props: {
-//             cloths: clothsResponse
-//         }
-//     }
-// }
 
 
 export default Cart
